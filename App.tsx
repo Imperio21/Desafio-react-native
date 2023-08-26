e7864bf915c01e83d97f9ead224ea3980a141e8b
isabelle.imperio <isabelle.imperio@csptech.com.br>
2023-08-26 16:50:31 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {Text, View, TextInput } from 'react-native';
+import { Container } from './App.styles';
+import axios from 'axios';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const [cep, setCep] = useState('')
+const [street, setStreet] = useState('')
+const [number, setNumber] = useState('')
+const [complement, setComplement] = useState('')
+const [neighborhood, setNeighborhood] = useState('')
+const [city, setCity] = useState('')
+const [uf, setUf] = useState('')
+
+const loadCep = async () => {
+//validação de cep antes de fazer a chamada para api, principio EARLY RETURNS/GUARD CLAUSES 
+  const isValidCep = cep.length === 8 
+  if (!isValidCep) return null 
+
+//o try só vai ocorrer caso o cep seja válido
+  try {
+    const { data } = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+    setStreet(data.logradouro)
+    setNeighborhood(data.bairro)
+    setCity(data.localidade)
+    setUf(data.uf)
+  } catch(error){
+    console.log(error)
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    loadCep()
+  },[cep])
+
+ return (
+    <Container>
+      <View>
+        <Text>CEP:</Text>
+        <TextInput
+          value={cep}
+          onChangeText={setCep}
+        />
+      </View>
+
+      <View>
+        <Text>Logradouro:</Text>
+          <TextInput
+            value={street}
+            onChangeText={setStreet}
+          />
+      </View>
+
+      <View>
+        <Text>Número:</Text>
+          <TextInput 
+            value={number}
+            onChangeText={setNumber}
+            />
+        </View>
+
+      <View>
+        <Text>Complemento:</Text>
+          <TextInput 
+            value={complement}
+            onChangeText={setComplement}
+          />
+      </View>
+    
+      <View>
+        <Text>Bairro:</Text>
+          <TextInput 
+            value={neighborhood}
+            onChangeText={setNeighborhood}
+          />
+      </View>
+
+      <View>
+        <Text>Cidade:</Text>
+          <TextInput
+            value={city}
+            onChangeText={setCity}
+          />
+      </View>
+
+      <View>
+        <Text>Estado:</Text>
+          <TextInput 
+            value={uf}
+            onChangeText={setUf}
+            />
+      </View>
+    </Container>
+ );
+}
+
